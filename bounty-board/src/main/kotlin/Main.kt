31f6f17cc2b04eb72cd213c0fd320d2dc4690056
strosu <rosu@outlook.com>
2023-04-20 15:13:55 +0300

    Hello world

---
+++ bounty-board/src/main/kotlin/Main.kt
@@ -0,0 +1,17 @@
+fun main() {
+    var jouneyStrted: Boolean = false
+
+    var playerName = "Ender"
+    var playerLevel: Int = 4
+    println("$playerLevel is called $playerName")
+
+    println("Kotlin journey starts here")
+
+    playerLevel += 1
+
+    println(playerLevel)
+
+    var itemsList = listOf("sword", "shield")
+
+    println(itemsList[1])
+}
